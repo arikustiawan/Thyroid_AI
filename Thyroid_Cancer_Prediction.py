7a0d185361f9154caf33631c3532bc01097042c5
arikustiawan <ariekustiawan@gmail.com>
2025-09-17 09:59:50 +0800

    Update Thyroid_Cancer_Prediction.py

--- Thyroid_Cancer_Prediction.py
+++ Thyroid_Cancer_Prediction.py
@@ -152,7 +152,10 @@ if st.button("Diagnose"):
         columns=["usg_composition", "usg_echogenicity", "usg_vascularity"]
     )
     df_encoded = pd.get_dummies(df_encoded).reindex(
-        columns=[f"usg_composition_{c}" for c in categories["usg_composition"]] +
+        columns=[["age", "gender","usg_tirads","usg_max_size_mm", "usg_shape", "usg_margins", "usg_calcifications",
+                  "usg_extrathyroidal_extension", "usg_suspicious_lymph_nodes", "fnac_nuclear_atypia",
+                  "fnac_colloid,"fnac_cellularity","tsh","calcitonin","cea","tg","tgab","nlr","plr"] +
+                 f"usg_composition_{c}" for c in categories["usg_composition"]] +
         [f"usg_echogenicity_{c}" for c in categories["usg_echogenicity"]] +
         [f"usg_vascularity_{c}" for c in categories["usg_vascularity"]],
         fill_value=0
